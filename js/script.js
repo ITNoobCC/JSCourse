07612bb7a2cfbf68b20e1b8bdcbdadc59f6bcbf4
Volodya <flipflike@bk.ru>
2021-07-19 15:23:50 +0300

    lesson 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert('123');
+let money = 10,
+    income = 'фриланс',
+    addExpenses = 'интернет, такси, комуналка',
+    desposit = true,
+    mission = 100,
+    period = 10;
+
+console.log(typeof(money), typeof(income), typeof(desposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split([]));
+
+let budgetDay = mission / 30;
+console.log(budgetDay);
